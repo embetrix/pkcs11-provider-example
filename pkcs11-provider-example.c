f1c2479cb7b014390e9077eb46de9f5e6e720fc2
Ayoub Zaki <ayoub.zaki@embetrix.com>
2025-01-16 19:06:41 +0100

    Add UI_method_set_reader to set PIN

--- pkcs11-provider-example.c
+++ pkcs11-provider-example.c
@@ -3,38 +3,50 @@
 #include <openssl/provider.h>
 #include <openssl/store.h>
 #include <openssl/core_names.h>
+#include <openssl/ui.h>
 
-int main(int argc, char *argv[]) {
+int ui_get_pin(UI *ui, UI_STRING *uis) {
+    const char *pin = (const char *)UI_get0_user_data(ui);
+    if (pin == NULL) {
+        return -1;
+    }
+    int ret = UI_set_result(ui, uis, pin);
+    return (ret == 0) ? 1 : -1;
+}
+
+EVP_PKEY *load_key_provider(const char *pkcs11_uri, const char *pin) {
     OSSL_PROVIDER *pkcs11_provider = NULL;
     OSSL_PROVIDER *default_provider = NULL;
     OSSL_STORE_CTX *store = NULL;
     OSSL_STORE_INFO *store_info = NULL;
     EVP_PKEY *pkey = NULL;
+    UI_METHOD *ui_method = NULL;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <pkcs11-uri>", argv[0]);
-        return 1;
-    }
-
-    const char *pkcs11_uri = argv[1];
     /* Load the default provider */
     default_provider = OSSL_PROVIDER_load(NULL, "default");
     if (!default_provider) {
-        fprintf(stderr, "Failed to load default provider");
-        return 1;
+        fprintf(stderr, "Failed to load default provider\n");
+        goto cleanup;
     }
 
     /* Load the PKCS#11 provider */
     pkcs11_provider = OSSL_PROVIDER_load(NULL, "pkcs11");
     if (!pkcs11_provider) {
-        fprintf(stderr, "Failed to load PKCS#11 provider");
-        OSSL_PROVIDER_unload(default_provider);
-        return 1;
+        fprintf(stderr, "Failed to load PKCS#11 provider\n");
+        goto cleanup;
     }
 
-    store = OSSL_STORE_open_ex(pkcs11_uri, NULL, "provider=pkcs11", NULL, NULL, NULL, NULL, NULL);
+    ui_method = UI_create_method("PIN reader");
+    if (!ui_method) {
+        fprintf(stderr, "Failed to create UI method\n");
+        goto cleanup;
+    } else {
+        UI_method_set_reader(ui_method, ui_get_pin);
+    }
+
+    store = OSSL_STORE_open_ex(pkcs11_uri, NULL, "provider=pkcs11", ui_method, (void *)pin, NULL, NULL, NULL);
     if (!store) {
-        fprintf(stderr, "Failed to open OSSL_STORE (check URI or provider setup)");
+        fprintf(stderr, "Failed to open OSSL_STORE (check URI or provider setup)\n");
         goto cleanup;
     }
 
@@ -45,39 +57,70 @@ int main(int argc, char *argv[]) {
             /* Extract the key */
             pkey = OSSL_STORE_INFO_get1_PKEY(store_info);
             if (pkey) {
-                /* Determine key type (RSA, EC, etc.) */
-                int base_id = EVP_PKEY_base_id(pkey);
-                if (base_id == EVP_PKEY_RSA) {
-                    printf("Loaded an RSA private key.\n");
-                } else if (base_id == EVP_PKEY_EC) {
-                    printf("Loaded an ECC private key.\n");
-                } else {
-                    fprintf(stderr, "Loaded a private key of unknown type (base_id=%d).\n", base_id);
-                }
-                EVP_PKEY_free(pkey);
-                pkey = NULL;
+                break;
             }
         }
-
         OSSL_STORE_INFO_free(store_info);
         store_info = NULL;
     }
 
-    if (!OSSL_STORE_eof(store)) {
-        fprintf(stderr, "Did not reach end of store.");
+    if (!pkey) {
+        fprintf(stderr, "Failed to load private key\n");
     }
 
 cleanup:
     if (store) {
         OSSL_STORE_close(store);
     }
-
     if (pkcs11_provider) {
         OSSL_PROVIDER_unload(pkcs11_provider);
     }
     if (default_provider) {
         OSSL_PROVIDER_unload(default_provider);
     }
+    if (ui_method) {
+        UI_destroy_method(ui_method);
+    }
 
-    return 0;
+    return pkey;
+}
+
+int main(int argc, char *argv[]) {
+    const char *pin = NULL;
+    int ret = -1;
+
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: %s <pkcs11-uri> [pkcs11-pin]\n", argv[0]);
+        return ret;
+    }
+
+    const char *pkcs11_uri = argv[1];
+    if (argc == 3) {
+        pin = argv[2];
+    }
+
+    printf("PKCS#11 URI: %s\n", pkcs11_uri);
+    if (pin) {
+        printf("PKCS#11 PIN: %s\n", pin);
+    } else {
+        printf("PKCS#11 PIN: not provided\n");
+    }
+
+    EVP_PKEY *key = load_key_provider(pkcs11_uri, pin);
+    if (key) {
+        int key_type = EVP_PKEY_base_id(key);
+        if (key_type == EVP_PKEY_RSA) {
+            printf("Loaded an RSA private key.\n");
+        } else if (key_type == EVP_PKEY_EC) {
+            printf("Loaded an ECC private key.\n");
+        } else {
+            fprintf(stderr, "Loaded a private key of unknown type (base_id=%d).\n", key_type);
+        }
+        EVP_PKEY_free(key);
+        ret = 0;
+    } else {
+        fprintf(stderr, "Failed to read private key\n");
+    }
+
+    return ret;
 }
--- pkcs11-provider-example_test.sh
+++ pkcs11-provider-example_test.sh
@@ -13,8 +13,8 @@ rm -rf .softhsm
 mkdir -p .softhsm/tokens
 echo "directories.tokendir = $PWD/.softhsm/tokens" > .softhsm/softhsm2.conf
 pkcs11-tool --pin $PIN --module $PKCS11_MODULE_PATH --slot-index=0 --init-token --label=$TOKEN_NAME --so-pin $SO_PIN --init-pin 
-pkcs11-tool --pin $PIN --module $PKCS11_MODULE_PATH  --keypairgen --key-type rsa:2048      --label "testRSAKey" --id 2
+pkcs11-tool --pin $PIN --module $PKCS11_MODULE_PATH  --keypairgen --key-type RSA:2048      --label "testRSAKey" --id 2
 pkcs11-tool --pin $PIN --module $PKCS11_MODULE_PATH  --keypairgen --key-type EC:prime256v1 --label "testECCKey" --id 3
 
 ./pkcs11-provider-example "pkcs11:object=testRSAKey;type=private?pin-value=12345"
-./pkcs11-provider-example "pkcs11:object=testECCKey;type=private?pin-value=12345"
+./pkcs11-provider-example "pkcs11:object=testECCKey;type=private" "12345"
